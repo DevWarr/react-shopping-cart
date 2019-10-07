d5eb2967137e578090bb6064ad651057671f8ebf
Devin Warrick <dev.warr@gmail.com>
2019-10-07 12:58:39 -0500

    Add test for ShoppingCartItem

---
+++ src/components/__tests__/ShoppingCartItem.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Item from "../ShoppingCartItem"
+import { render, fireEvent } from "@testing-library/react";
+
+test("<Product/> loads properly when passed props", () => {
+    const product = {title: "John's", price: "12.99"}
+    const cons = () => console.log("click")
+    const shoppingItem = render(<Item {...product} removeItem={cons}/>)
+
+    shoppingItem.getByText(/john/i)
+    shoppingItem.getByAltText(/john/i)
+    shoppingItem.getByText(/12\.99/i)
+    const button = shoppingItem.getByText(/remove/i)
+    fireEvent.click(button)
+})
